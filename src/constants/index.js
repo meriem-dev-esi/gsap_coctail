fa0de88328aabbd1b3e86aaebde199fc775c0d82
meriem-dev-esi <mm_chennaf@esi.dz>
2026-08-29 00:02:47 +0100

    implementation  of  hero  section

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const navlinks = [
+    { id: 'cocktails', title: 'Cocktails' },
+    { id: 'about', title: 'About us' },
+    { id: 'The Art', title: 'The Art' },
+    { id: 'contact', title: 'Contact Us' },
+];

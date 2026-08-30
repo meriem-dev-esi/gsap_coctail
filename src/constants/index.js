673529d20501b275efbb407bc9cbbaa7b06d64bc
meriem-dev-esi <mm_chennaf@esi.dz>
2026-08-30 12:20:50 +0100

    implement Cocktails Section

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,72 @@ export const navlinks = [
     { id: 'The Art', title: 'The Art' },
     { id: 'contact', title: 'Contact Us' },
 ];
+
+export const cocktailLists1 = [
+    {
+        name: "Chapel Hill Shiraz",
+        country: "AU",
+        detail: "Bottle",
+        price: "$56",
+    },
+    {
+        name: "Catena Malbec",
+        country: "AR",
+        detail: "Bottle",
+        price: "$59",
+    },
+    {
+        name: "La Vieille Ferme",
+        country: "FR",
+        detail: "750 ml",
+        price: "$20",
+    },
+    {
+        name: "Rhino Pale Ale",
+        country: "CA",
+        detail: "750 ml",
+        price: "$31",
+    },
+    {
+        name: "Irish Guinness",
+        country: "IE",
+        detail: "750 ml",
+        price: "$26",
+    },
+];
+
+export const cocktailLists2 = [
+    {
+        name: "Aperol Spritz",
+        country: "IT",
+        detail: "350 ml",
+        price: "$20",
+    },
+    {
+        name: "Dark 'N' Stormy",
+        country: "BM",
+        detail: "450 ml",
+        price: "$16",
+    },
+    {
+        name: "Daiquiri",
+        country: "CU",
+        detail: "350 ml",
+        price: "$10",
+    },
+    {
+        name: "Old Fashioned",
+        country: "US",
+        detail: "350 ml",
+        price: "$31",
+    },
+    {
+        name: "Negroni",
+        country: "IT",
+        detail: "350 ml",
+        price: "$26",
+    },
+];
+
+// Alias for backwards compatibility if referenced as cocktailLists
+export const cocktailLists = cocktailLists1;
